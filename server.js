d1a000f2b6b5e9ed7643a2037d21949330db4690
Andre Frankenthal <afrankenthal@gmail.com>
2016-01-18 23:42:49 +0000

    first version

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+router.use(bodyParser.json());
+
+router.use(function(req,res,next) {
+
+    console.log(req.method, req.url);
+
+    next();
+});
+
+router.get('/get/json', function(req,res) {
+    var monitorData = fs.readFileSync('/media/card/json/monitorData.json');
+    var obj = JSON.parse(monitorData);
+    res.json(obj);
+});
+
+router.get('/get/controlJSON', function(req,res) {
+    var initialControlData = fs.readFileSync('/media/card/json/controlData.json');
+    var obj = JSON.parse(initialControlData);
+    console.log(obj);
+    res.json(obj);
+});
+
+router.post('/post/json', function(req,res) {
+    console.log(req.body);
+    fs.writeFileSync('/media/card/json/controlData.json', JSON.stringify(req.body));
+    res.send({});
+});
+
+router.use(express.static(__dirname + '/public'));
+
+app.use('/', router);
+
+app.listen(8081, function() {
+    console.log('Listening in port 8081...');
+})
